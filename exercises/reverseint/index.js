b6d058f5e745430aecf6b00de4c0b55ce294e14b
Jacob <jerling1989@gmail.com>
2018-06-06 03:55:13 -0400

    max character in string

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,8 +9,8 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-	const reversed = n.toString().split('').reverse().join('');
-	return parseInt(reversed) * Math.sign(n);
+	const reverse = n.toString().split('').reverse().join('');
+	return parseInt(reverse) * Math.sign(n);
 }
 
 
