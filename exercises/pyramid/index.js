948d9d11b04c58f7461095e77bbda261f908ce0d
Jacob <jerling1989@gmail.com>
2018-06-08 04:03:04 -0400

    video solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -24,7 +24,29 @@ function pyramid(n) {
 }
 
 
+// VIDEO SOLUTION
+function pyramid(n) {
+	const midpoint = Math.floor((2*n-1) / 2);
 
-pyramid(10);
+	for (let row = 0; row < n; row++) {
+		let level = '';
+
+		for (let col = 0; col < 2*n-1; col++) {
+			if (midpoint - row <= col && midpoint + row >= col) {
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+		console.log(level);
+	}
+}
+
 
 module.exports = pyramid;
+
+
+
+
+
+
